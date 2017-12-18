5f3c5c5c8252d41989fd2507d941e5b7106e63d7
github username <github email address>
2017-12-18 18:43:46 -0500

    Finished the spec.md and the README

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
+## Description
 
+If you do something one day that you know is good for you, start doing it once a week. Maybe start doing it twice a week. Don't bug yourself out about getting it done. Let the good habit unfold without the stress of building it.
+
+Write down why you would want to do it again, so next time, in case you don't feel it, you'll still jump in. Get that habit built.
+
+## Install Instructions
+
+For now, clone the repository, (download the folder containing all the code to you machine,) `cd` to the directory, (open a command prompt window and make sure you navigate to the folder holding all the code,) and run the shotgun gem!
+
+This will launch a server on your machine, go there, and signup. Then you can see your homepage which is a week view of tasks that you can add. The sidebar has relevant links depending on where in the app you find yourself.
+
+## Contributing
+
+Please feel free to make a pull request. I will check it out. Contributions most welcome.
+
+## License
+
+Copyright 2017 Nikita (Yehuda) Makarov
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,3 @@
-
-
-user = User.create(username: "testuser", email: "testuser@gmail.com", password: "password")
-
-fake_user = User.create(username: "fakeuser", email: "fakeuser@gmail.com", password: "password")
-
 sunday = Day.create(name: "sunday")
 monday = Day.create(name: "monday")
 tuesday = Day.create(name: "tuesday")
@@ -11,27 +5,3 @@ wednesday = Day.create(name: "wednesday")
 thursday = Day.create(name: "thursday")
 friday = Day.create(name: "friday")
 saturday = Day.create(name: "saturday")
-
-task = Task.create(name: "eat a good breakfast", effect: "I'll be energetic!", user: user)
-
-task.day_tasks.create(day: sunday)
-task.day_tasks.create(day: monday)
-
-
-Task.create(name: "be happy 1", effect: "this is amazing", user: user).day_tasks.create(day: sunday)
-Task.create(name: "be happy 1.1", effect: "this is amazing", user: user).day_tasks.create(day: monday)
-Task.create(name: "be happy 1.2", effect: "this is amazing", user: user).day_tasks.create(day: tuesday)
-Task.create(name: "be happy 2", effect: "this is amazing", user: user).day_tasks.create(day: wednesday)
-Task.create(name: "be happy 3", effect: "this is amazing", user: user).day_tasks.create(day: thursday)
-Task.create(name: "be happy 4", effect: "this is amazing", user: user).day_tasks.create(day: friday)
-Task.create(name: "be happy 5", effect: "this is amazing", user: user).day_tasks.create(day: saturday)
-Task.create(name: "be happy 6", effect: "this is amazing", user: user).day_tasks.create(day: sunday)
-
-Task.create(name: "be amazing 1", effect: "this is amazing", user: fake_user).day_tasks.create(day: sunday)
-Task.create(name: "be amazing 1.1", effect: "this is amazing", user: fake_user).day_tasks.create(day: sunday)
-Task.create(name: "be amazing 1.2", effect: "this is amazing", user: fake_user).day_tasks.create(day: sunday)
-Task.create(name: "be amazing 2", effect: "this is amazing", user: fake_user).day_tasks.create(day: sunday)
-Task.create(name: "be amazing 3", effect: "this is amazing", user: fake_user).day_tasks.create(day: sunday)
-Task.create(name: "be amazing 4", effect: "this is amazing", user: fake_user).day_tasks.create(day: tuesday)
-Task.create(name: "be amazing 5", effect: "this is amazing", user: fake_user).day_tasks.create(day: sunday)
-Task.create(name: "be amazing 6", effect: "this is amazing", user: fake_user).day_tasks.create(day: monday)
